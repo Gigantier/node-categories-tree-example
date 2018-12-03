de7d55a167788297dec9d49532012d6d0f6de511
Emiliano Mettini <mettini@gmail.com>
2018-12-03 11:00:57 -0300

    babel + es6 + multi-level tree

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-const request = require('request-promise');
-const querystring = require('querystring');
-const groupBy = require('lodash/groupBy');
+import "isomorphic-fetch";
 
-const CATEGORY_PRINT_PREFIX = '    --> ';
 const apiHost = process.env.API_HOST;
 const apiPrefix = '/api/v1';
 const tokenUri = `${apiPrefix}/OAuth/token`;
 const categoriesUri = `${apiPrefix}/Menu/list`;
-const tokenRequestBody = querystring.stringify({
+
+const tokenRequestBody = {
   'client_id': process.env.API_CLIENT_ID,
   'client_secret': process.env.API_CLIENT_SECRET,
   'scope': process.env.API_SCOPE,
   'grant_type': 'client_credentials'
-});
+};
 
 function post(url, body) {
-  return request({
-    method: 'POST',
-    uri: url,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: body
-  }).then(JSON.parse);
+  return fetch(url, { method: 'POST', body: JSON.stringify(body) }).then(r => r.json());
 }
 
 function getApiToken() {
@@ -30,33 +21,20 @@ function getApiToken() {
 }
 
 function getCategories(apiToken) {
-  return post(apiHost + categoriesUri, querystring.stringify({
-    'access_token': apiToken,
-    'all': true
-  })).then(result => result.menues);
+  return post(apiHost + categoriesUri, { 'access_token': apiToken, 'all': true }).then(result => result.menues);
 }
 
-function buildUIData(categories) {
-  let map = categories.reduce((cs, c) => { cs[c.id] = c; return cs }, {});
-  let groups = groupBy(categories, category => category.parentId);
-
-  return groups[null].map(c => Object.assign({}, { 
-    category: map[c.id], 
-    children: typeof groups[c.id]!== 'undefined' ? groups[c.id] : [] 
-  }));
-}
-
-function getUIData() {
-  return getApiToken().then(getCategories).then(buildUIData);
-}
-
-function printUIData(uiData) {
-  console.log(uiData.category.name);
-  uiData.children.forEach(c => console.log(CATEGORY_PRINT_PREFIX + c.name));
+function printUIData(groups, [n, ...others], prefix) {
+  if (typeof n !== 'undefined') {
+	  console.log(`${prefix}> ${n.name}`);
+	  printUIData(groups, (groups[n.id] || []), `${prefix}  `);
+	  printUIData(groups, others, prefix);	
+  }
 }
 
 // --
 
-getUIData()
-  .then(data => data.forEach(printUIData))
-  .catch(err => console.error('Cannot get ui data.', err));
+getApiToken().then(getCategories).then(data => {
+  const groups = data.reduce((ns, n) => { (ns[n.parentId] = ns[n.parentId] || []).push(n); return ns; }, {});
+  printUIData(groups, groups[null], '');
+}).catch(err => console.error('Cannot get ui data.', err));
